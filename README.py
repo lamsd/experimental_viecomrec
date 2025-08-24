2bf9f44390c89727ce5b55320ee3e4e32e16feb0
lamsd <huynhlam14091994@gmail.com>
2025-08-24 12:52:18 +0700

    firstly

--- README.py
+++ README.py
@@ -1,1 +1,7 @@
 # Experimental_VietComRec
+Thông tin bải báo VietComRec.
+1. Paper: https://doras.dcu.ie/29693/1/viecomrec.pdf
+2. Repo: https://github.com/linh222/face_cleanser_recommendation_dataset
+3. Dataset: https://drive.google.com/drive/folders/1AvR71nHxK81DEtwFBAJzYDyBAo
+
+Quá trình xử lý
